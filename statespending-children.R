dd8b186edcf83640391969aad7cb6653dcdbab31
miichellyy <106936637+miichellyy@users.noreply.github.com>
2022-11-08 19:08:21 -0800

    Create statespending-children.R

---
+++ statespending-children.R
@@ -0,0 +1,6 @@
+setwd("~/Documents/School/MICA/Design Lab/Final Project/Data")
+
+require(tidyverse)
+require(readxl)
+
+State_by_State_Spending_on_Kids <- read_excel("raw data/State-by-State Spending on Kids.xlsx")
