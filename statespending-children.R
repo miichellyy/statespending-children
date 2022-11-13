515153ac984952d27221d01a66d50876ad9d7c4a
miichellyy <106936637+miichellyy@users.noreply.github.com>
2022-11-13 10:28:18 -0800

    exports & updates
    created export of R analysis. Added abortion access score csv.

--- statespending-children.R
+++ statespending-children.R
@@ -4,20 +4,22 @@
 # Script set up
 require(tidyverse)
 require(dplyr)
-require(GGally)
+require(reshape)
 
-setwd("~/Documents/School/MICA/Design Lab/Final Project/Data")
+# make sure to set your working directory to the folder that contains the csv referenced below.
+
 
 # Load data file & set states as factor
 stateChildSpending <- read.csv("ChildcareSpending_Metrics.csv")
 stateChildSpending$State <- as.factor(stateChildSpending$State)
 
-summary(stateChildSpending) #looks like NY has a negative value for edservs variable. 
+summary(stateChildSpending)
 
 #identify any na values
 sapply(stateChildSpending, function(x) sum(is.na(x)))
 
 #explore select spending levels using bar charts. Captions from data dictionary
+library(GGally)
 ggpairs(stateChildSpending,
         columns = 2:22)
 
@@ -29,20 +31,6 @@ ggplot(stateChildSpending, aes(y=reorder(State, PK12ed_realperch), x=PK12ed_real
        description for source details.")+
   ylab("State")
 
-ggplot(stateChildSpending, aes(y=reorder(State, pell_realperch), x=pell_realperch))+
-  theme_classic()+
-  geom_col(fill='cadetblue4')+
-  xlab("Public spending on Pell Grants per child ages 0-18 by state and year, 
-       adjusted for inflation using a GDP deflator. See pell description for source details. ")+
-  ylab("State")
-
-ggplot(stateChildSpending, aes(y=reorder(State, headstartpriv_realperch), x=headstartpriv_realperch))+
-  theme_classic()+
-  geom_col(fill='cadetblue4')+
-  xlab("Federal spending on Head Start awarded to private grantees per child ages 
-       0-18 by state and year, adjusted for inflation using a GDP deflator. See 
-       HeadStart description for source details.")+
-  ylab("State")
 
 ggplot(stateChildSpending, aes(y=reorder(State, SNAP_realperch), x=SNAP_realperch))+
   theme_classic()+
@@ -78,12 +66,6 @@ ggplot(stateChildSpending, aes(y=reorder(State, stateEITC_realperch), x=stateEIT
 #melt all raw inputs in to 1 long table
 stateChildSpendingLong <- melt(stateChildSpending, id.vars = c("State"))
 
-#explore data as barchat small multiples
-ggplot(stateChildSpendingLong, aes(y=State, x=value))+
-  theme_classic()+
-  geom_col()+
-  facet_wrap(~variable)
-
 #explore data as histogram small multiples
 ggplot(data = stateChildSpendingLong, aes(x = value)) + geom_histogram()+
   facet_wrap(~variable)
@@ -91,56 +73,87 @@ ggplot(data = stateChildSpendingLong, aes(x = value)) + geom_histogram()+
 ### calculate total spending by categories
 
 #education spending per capita
-stateChildSpending$education <- with(stateChildSpending,PK12ed_realperch + highered_realperch + edservs_realperch + pell_realperch + headstartpriv_realperch)
+stateChildSpending$education <- with(stateChildSpending,
+                                     PK12ed_realperch+
+                                      highered_realperch+
+                                      edservs_realperch+
+                                      pell_realperch+
+                                      headstartpriv_realperch)
 
-#nutrition and social services spending per capita  
-stateChildSpending$nutrition_social <- with(stateChildSpending,othercashserv_realperch + SNAP_realperch)
+#financial assistance spending per capita  
+stateChildSpending$financial <- with(stateChildSpending,
+                                            othercashserv_realperch+
+                                            TANFbasic_realperch)
+
+#nutrition spending per capita  
+stateChildSpending$nutrition <- stateChildSpending$SNAP_realperch
 
 #social security spending per capita
-stateChildSpending$soc_security <- with(stateChildSpending, socsec_realperch + fedSSI_realperch)
+stateChildSpending$soc_security <- with(stateChildSpending,
+                                        socsec_realperch+
+                                        fedSSI_realperch)
 
 #tax credits spending per capita
-stateChildSpending$tax <- with(stateChildSpending, fedEITC_realperch + CTC_realperch + addCC_realperch + stateEITC_realperch)
+stateChildSpending$tax <- with(stateChildSpending,
+                              fedEITC_realperch+
+                              CTC_realperch+
+                              addCC_realperch+
+                              stateEITC_realperch)
 
 #healthcare spending per capita
-stateChildSpending$health <- with(stateChildSpending, Medicaid_CHIP_realperch + pubheath_realperch + other_health_realperch)
+stateChildSpending$health <- with(stateChildSpending,
+                                  Medicaid_CHIP_realperch+
+                                  pubheath_realperch+
+                                  other_health_realperch)
 
 #housing spending per capita
 stateChildSpending$housing <- stateChildSpending$HCD_realperch
 
 #public resources spending per capita
-stateChildSpending$resources <- with(stateChildSpending, lib_realperch + parkrec_realperch)
+stateChildSpending$resources <- with(stateChildSpending,
+                                    lib_realperch+
+                                    parkrec_realperch)
+
+#employment benefits spening per capita
+stateChildSpending$employment <- with(stateChildSpending,
+                                      wcomp_realperch+
+                                      unemp_realperch)
 
 #create new dataframe with totals
-childSpendingTotals <- data.frame(stateChildSpending$State, stateChildSpending$education, stateChildSpending$nutrition_social, 
-                                  stateChildSpending$soc_security, stateChildSpending$tax, stateChildSpending$health, 
-                                  stateChildSpending$housing, stateChildSpending$resources)
+stateChildSpendingTotals <- data.frame(stateChildSpending$State,
+                                  stateChildSpending$education,
+                                  stateChildSpending$financial,
+                                  stateChildSpending$nutrition,
+                                  stateChildSpending$soc_security,
+                                  stateChildSpending$tax,
+                                  stateChildSpending$health, 
+                                  stateChildSpending$housing,
+                                  stateChildSpending$resources,
+                                  stateChildSpending$employment)
 
 #rename columns
-names(childSpendingTotals)[1] <- "state"
-names(childSpendingTotals)[2] <- "education"
-names(childSpendingTotals)[3] <- "nutrition_social"
-names(childSpendingTotals)[4] <- "soc_security"
-names(childSpendingTotals)[5] <- "tax"
-names(childSpendingTotals)[6] <- "health"
-names(childSpendingTotals)[7] <- "housing"
-names(childSpendingTotals)[8] <- "resources"
+names(stateChildSpendingTotals)[1] <- "state"
+names(stateChildSpendingTotals)[2] <- "education"
+names(stateChildSpendingTotals)[3] <- "financial"
+names(stateChildSpendingTotals)[4] <- "nutrition"
+names(stateChildSpendingTotals)[5] <- "soc_security"
+names(stateChildSpendingTotals)[6] <- "tax"
+names(stateChildSpendingTotals)[7] <- "health"
+names(stateChildSpendingTotals)[8] <- "housing"
+names(stateChildSpendingTotals)[9] <- "resources"
+names(stateChildSpendingTotals)[10] <- "employment"
 
-
-#melt for analytics
-require(reshape2)
-
-chilSpendingTotalsLong <- melt(childSpendingTotals, id.vars = c("state"))
+stateChildSpendingTotalsLong <- melt(stateChildSpendingTotals, id.vars = c("state"))
 
 #total spending
-ggplot(chilSpendingTotalsLong, aes(y=reorder(state, value), x=value))+
+ggplot(stateChildSpendingTotalsLong, aes(y=reorder(state, value), x=value))+
   theme_classic()+
   geom_col(fill='cadetblue4')+
   xlab("total spending per child ($)")+
   ylab("state")
 
 #total spending per category
-ggplot(chilSpendingTotalsLong, aes(y=reorder(state, value), x=value, fill = variable))+
+ggplot(stateChildSpendingTotalsLong, aes(y=reorder(state, value), x=value, fill = variable))+
   theme_classic()+
   geom_col()+
   xlab("total spending per child ($)")+
@@ -148,29 +161,14 @@ ggplot(chilSpendingTotalsLong, aes(y=reorder(state, value), x=value, fill = vari
   guides(fill=guide_legend("spending category"))
 
 #percent of total spending
-ggplot(chilSpendingTotalsLong, aes(y=reorder(state, value), x=value, fill = variable))+
+ggplot(stateChildSpendingTotalsLong, aes(y=reorder(state, value), x=value, fill = variable))+
   theme_classic()+
   geom_col(position = "fill")+
   xlab("% of total spending")+
   ylab("state")+
   guides(fill=guide_legend("spending category"))
 
-#calculate ranks
-childSpendingTotals$educationRank <- rank(childSpendingTotals$education)
-childSpendingTotals$nutrition_socialRank <- rank(childSpendingTotals$nutrition_social)
-childSpendingTotals$soc_securityRank <- rank(childSpendingTotals$soc_security)
-childSpendingTotals$taxRank <- rank(childSpendingTotals$tax)
-childSpendingTotals$healthRank <- rank(childSpendingTotals$health)
-childSpendingTotals$housingRank <- rank(childSpendingTotals$housing)
-childSpendingTotals$resourcesRank <- rank(childSpendingTotals$resources)
-
-#calculate overallRank
-childSpendingTotals$finalRank <- with(childSpendingTotals, educationRank + nutrition_socialRank +soc_securityRank + taxRank + healthRank + housingRank + resourcesRank)
-
-ggplot(childSpendingTotals, aes(y=reorder(state, finalRank), x=finalRank))+
-  theme_classic()+
-  geom_col(fill='cadetblue4')+
-  xlab("points")+
-  ylab("state")
+#export results a csv to use in GIS
+write.csv(stateChildSpendingTotals,"stateChildSpendingTotals.csv", row.names = FALSE)
 
 
